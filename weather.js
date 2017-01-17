71a7a8848bae9e3f407938fe90d85b7b8c49a494
shenoyabhijith <abhijith2831@gmail.com>
2017-01-17 19:13:17 +0530

    removed bugs app complete animation incomplete

--- weather.js
+++ weather.js
@@ -1,39 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var api = "https://api.wunderground.com/api/19b1920430424fb2/geolookup/q/";
-  var forecast = "https://api.wunderground.com/api/19b1920430424fb2/forecast/q/CA/";
+  var api = "https://api.wunderground.com/api/19b1920430424fb2/conditions/forecast/alert/q/";
+  var forecast = 'https://api.wunderground.com/api/19b1920430424fb2/forecast/q/';
   // determine geo lookup
+
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       console.log(position.coords.latitude + "\t" + position.coords.longitude);
       api = api + position.coords.latitude + "," + position.coords.longitude + ".json?callback=?";
       console.log(api);
       var $content = $("#cntnt");
-      $.getJSON(api, function(weather) {
-        $content.append("<h3>airport:"+"<br/>" + weather.location.nearby_weather_stations.airport.station[0].city+"<br/>");
-        $content.append("<b>Country</b> :"+ weather.location.country);      
-
+      $.getJSON(api, function (weather) {
         console.log(weather);
-        forecast = forecast+weather.location.city+".json";
-        console.log(forecast);
-        $.getJSON(forecast,function(foreCast){
-          console.log(foreCast)
-         var hstTmpF =  foreCast.forecast.simpleforecast.forecastday[0].high.fahrenheit;
-          var hstTmpC =foreCast.forecast.simpleforecast.forecastday[0].high.celsius;
-          var lstTmpF = foreCast.forecast.simpleforecast.forecastday[0].low.fahrenheit;
-          var lstTmpC =  foreCast.forecast.simpleforecast.forecastday[0].low.celsius;
-   /*       var HmessageF = "<br/><b>Highest Temparature in fahrenheit</b> :";
-          var LmessageF = "<br/><b>Lowest Temparature in fahrenheit</b> :";
-          var LmessageC = "<br/><b>Lowest Temparature in celcius:</b>";
-          var HmessageC = "<br/><b>Highest Temparature in celcius:</b>"; */
-          $content.append("<br/><b>Weather description</b> :" + foreCast.forecast.txt_forecast.forecastday[0].fcttext); 
-          $content.append("<br/><b>TIME ZONE</b> :" + foreCast.forecast.simpleforecast.forecastday[0].date.tz_long);   
-  /*       $content.append(HmessageF + hstTmpF+"&degF" +HmessageC+hstTmpC+"&degC");        
-          $content.append(LmessageF + lstTmpF+"&degF" +LmessageC+lstTmpC+"&degC");    */       
-          $("#w-image img").prop("src",foreCast.forecast.txt_forecast.forecastday[0].icon_url);
-        });
-      });
-    });
+        var temp = weather.current_observation.temp_c ;
+        $("#w-image img").prop("src", weather.forecast.txt_forecast.forecastday[0].icon_url);
+        $content.append("<h4> " +temp + "°</h4>");
+        $content.append("<h3>" + weather.current_observation.observation_location.full + "</h3>");
+        $content.append("<h3>" + weather.current_observation.display_location.state_name + "</h3>");
+        $content.append(weather.forecast.txt_forecast.forecastday[0].fcttext);
+        console.log(weather);
+ if(temp<30){
+   var imgUrl = "https://s29.postimg.org/i2drvpck7/Cold_Weather_Backgrounds_Cold_weather_in_the_par.jpg";
+   document.body.style.backgroundImage = 'url(' + imgUrl + ')';
+ }
+  
+      })
+    })
   }
 
 })
